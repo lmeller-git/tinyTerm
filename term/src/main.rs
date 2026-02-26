ae78d7ab97bd142550d4561b5b6610045b1e7f32
lmeller-git <louis.meller@icloud.com>
2026-02-26 16:12:15 +0100

    update shell name

--- term/src/main.rs
+++ term/src/main.rs
@@ -57,7 +57,7 @@ pub fn main() -> Result<(), ProcessError> {
 
     println!("terminal hooked into serial, attached fb");
 
-    let shell = "/ram/bin/tinyShell.out";
+    let shell = "/ram/bin/tinyShell";
 
     let (pipes, shell_id) = init(shell);
 
